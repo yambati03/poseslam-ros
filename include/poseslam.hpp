8bc50838a0407aa1727ec3ff71c99e3168f973b1
Yashas Ambati <yashas.amb@gmail.com>
2025-04-17 01:51:14 -0400

    Get everything to build, more progress

--- include/poseslam.hpp
+++ include/poseslam.hpp
@@ -4,7 +4,12 @@
 #include <mutex>
 #include <iostream>
 #include <queue>
+#include <vector>
+
 #include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 
 #include <gtsam/geometry/Rot3.h>
 #include <gtsam/geometry/Pose3.h>
@@ -17,7 +22,41 @@
 #include <gtsam/nonlinear/ISAM2.h>
 #include <gtsam/linear/NoiseModel.h>
 
-#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/eigen.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/filter.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/pcl_macros.h>
+
+#include <Eigen/Dense>
+
+struct PointTypePose
+{
+    PCL_ADD_POINT4D; // PCL macro to add x, y, and z fields to the custom point type
+    float intensity;
+    float roll, pitch, yaw;
+    double time;
+
+    PointTypePose()
+        : x(0), y(0), z(0), intensity(0), roll(0), pitch(0), yaw(0), time(0) {}
+
+    PointTypePose(float _x, float _y, float _z, float _intensity,
+                  float _roll, float _pitch, float _yaw, double _time)
+        : x(_x), y(_y), z(_z), intensity(_intensity),
+          roll(_roll), pitch(_pitch), yaw(_yaw), time(_time) {}
+
+    PointTypePose(const gtsam::Pose3 &pose, float _intensity, double _time)
+        : x(pose.translation().x()), y(pose.translation().y()), z(pose.translation().z()),
+          intensity(_intensity),
+          roll(pose.rotation().roll()), pitch(pose.rotation().pitch()), yaw(pose.rotation().yaw()),
+          time(_time) {}
+};
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(PointTypePose,
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(float, roll, roll)(float, pitch, pitch)(float, yaw, yaw)(double, time, time))
 
 namespace poseslam
 {
@@ -31,16 +70,30 @@ namespace poseslam
         void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
         void add_pose_constraint(Eigen::Matrix<double, 4, 4> H);
 
+        void append_to_path(const PointTypePose &pose_in);
+        gtsam::Pose3 point_type_to_gtsam_pose(PointTypePose &pose_in);
+
         std::mutex pBufLock;
         std::queue<Eigen::MatrixXd> pointcloudBuf;
         std::queue<Eigen::Matrix<double, 4, 4>> transformBuf;
 
-        gtsam::ISAM2 *gisam;
-        gtsam::Values gisamCurrentEstimate;
-        gtsam::ISAM2Params gparameters;
+        gtsam::ISAM2 *isam;
+        gtsam::Values isam_initial_estimate;
+        gtsam::Values isam_current_estimate;
+        gtsam::ISAM2Params isam_params;
         gtsam::NonlinearFactorGraph f_graph_;
 
+        // We hold key poses in a PCL pointcloud type since PCL has nice helper functions
+        // to construct a KD tree from a pointcloud. This allows us to efficiently query the
+        // spatial distance between two poses.
+        pcl::PointCloud<pcl::PointXYZ>::Ptr key_poses_3d;
+        pcl::PointCloud<PointTypePose>::Ptr key_poses_6d;
+        PointTypePose last_pose_;
+
+        nav_msgs::msg::Path global_path;
+
         rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_sub;
+        rclcpp::Publisher<nav_msgs::msg::Path>::SharedPtr path_pub;
     };
 } // namespace poseslam
 
--- src/poseslam.cpp
+++ src/poseslam.cpp
@@ -1,11 +1,7 @@
 #include "poseslam.hpp"
 #include "simpleicp.h"
 
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/filters/filter.h>
-#include <pcl/PCLPointCloud2.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 /**
  * This is an example ROS 2 node that implements a simple pose SLAM algorithm using GTSAM.
@@ -61,14 +57,51 @@ namespace poseslam
 
             // Save the point cloud data to the queue
             pBufLock.lock();
-            pointcloudBuf.push(X_mov);
+            pointcloudBuf.push(X_fix);
             pBufLock.unlock();
         }
     }
 
     void PoseSlam::add_pose_constraint(Eigen::Matrix<double, 4, 4> H)
     {
-        gtsam::noiseModel::Diagonal::shared_ptr noise_model = gtsam::noiseModel::Diagonal::Variances((gtsam::Vector(6) << 1e-6, 1e-6, 1e-6, 1e-4, 1e-4, 1e-4).finished());
+        if (key_poses_3d->points.empty())
+        {
+            gtsam::noiseModel::Diagonal::shared_ptr prior_noise = gtsam::noiseModel::Diagonal::Variances((gtsam::Vector6(6) << 1e-2, 1e-2, M_PI * M_PI, 1e4, 1e4, 1e4).finished());
+            auto prior_pose = gtsam::Pose3(H);
+            f_graph_.add(gtsam::PriorFactor<gtsam::Pose3>(0, prior_pose, prior_noise));
+            isam_initial_estimate.insert(0, prior_pose);
+        }
+        else
+        {
+            gtsam::noiseModel::Diagonal::shared_ptr noise_model = gtsam::noiseModel::Diagonal::Variances((gtsam::Vector(6) << 1e-6, 1e-6, 1e-6, 1e-4, 1e-4, 1e-4).finished());
+            f_graph_.add(gtsam::BetweenFactor<gtsam::Pose3>(key_poses_3d->size() - 1, key_poses_3d->size(), gtsam::Pose3(H), noise_model));
+
+            gtsam::Pose3 lp = point_type_to_gtsam_pose(last_pose_);
+            isam_initial_estimate.insert(key_poses_3d->size(), gtsam::Pose3(H * lp.matrix()));
+        }
+    }
+
+    void PoseSlam::append_to_path(const PointTypePose &pose_in)
+    {
+        geometry_msgs::msg::PoseStamped msg;
+        msg.header.stamp = rclcpp::Time(pose_in.time);
+        msg.pose.position.x = pose_in.x;
+        msg.pose.position.y = pose_in.y;
+        msg.pose.position.z = pose_in.z;
+
+        tf2::Quaternion q;
+        q.setRPY(pose_in.roll, pose_in.pitch, pose_in.yaw);
+        msg.pose.orientation.x = q.x();
+        msg.pose.orientation.y = q.y();
+        msg.pose.orientation.z = q.z();
+        msg.pose.orientation.w = q.w();
+
+        global_path.poses.push_back(msg);
+    }
+
+    gtsam::Pose3 PoseSlam::point_type_to_gtsam_pose(PointTypePose &pose_in)
+    {
+        return gtsam::Pose3(gtsam::Rot3::Ypr(pose_in.yaw, pose_in.pitch, pose_in.roll), gtsam::Point3(pose_in.x, pose_in.y, pose_in.z));
     }
 }
 
